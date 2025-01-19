8d5ef3ef4728d53012cd07b48643c28714f05e34
Your Name <omit8998@gmail.com>
2025-01-20 01:17:51 +0600

    second update for shortcode

--- revise_3.php
+++ revise_3.php
@@ -1,6 +1,6 @@
 <?php
 /**
-*Plugin Name: Advance-qr-code
+*Plugin Name: setting_API
 *Plugin URI: https://alfanet.bd.org
 *Description:This is the plugin which will be advance function then previous.
 *Version: 1.2
@@ -16,34 +16,77 @@ function aqrc_load_textdomain(){
 };
 add_action("plugins_loaded","aqrc_load_textdomain");
 
-function aqrc_function($content){
-    $aqrc_post_link = get_permalink();
-    $aqrc_title = 'out put of the fuction';
-    $aqrc_src = sprintf('https://api.qrserver.com/v1/create-qr-code/?size=185x185&ecc=L&qzone=1&data=%s',$aqrc_post_link);
-    $content .= sprintf("<img src='%s'>",$aqrc_src);
-    return $content;
+function function_for_settings_api(){
+    add_settings_field( 'header', __('Header','aqrc'), 'function_for_setting_fileds', 'general');   
+    register_setting( 'general', 'header',array('sanaitze_callback'=>'esc_attr'));
 }
-add_filter( 'the_content', 'aqrc_function');
 
-function aqrc_settings_init(){
-
-    add_settings_field( 'aqrc_height', __('QR_code_Height','aqrc'),'aqrc_display_height','general');
-    add_settings_field( 'aqrc_width', __('QR_code_Width','aqrc'),'aqrc_display_width','general');
+function function_for_setting_fileds(){
     
-    register_setting('general','pqrc_display_height',array('sanaitze_callback'=>'esc_attr'));
-    register_setting('general','pqrc_display_width',array('sanaitze_callback'=>'esc_attr'));
-
+    ?>
+    <input type="text" class="regular-text" name="header" value="<?php echo get_option( 'header')?>"/>
+    <?php
 }
-function aqrc_display_height(){
-    $height = get_option( 'aqrc_height');
-    printf("<input type= 'text' id='%s' name='%s' value='%s'/>",'aqrc_height','aqrc_heith',$height);
+add_action( 'admin_init', 'function_for_settings_api');
+
+
+function button_function($attribute){
+ return sprintf( '<a href="%s">%s</a>',
+ $attribute['url'],
+ $attribute['title']
+
+ );
+}
+add_shortcode('btn', 'button_function');
+function button_functions(){
+return sprintf(
+    '<a href="%s">%s</a>',
+     $attribute['url'],
+     $attribute['title']
+);
 };
-function aqrc_display_width(){
-    $width = get_option( 'aqrc_width');
-    printf("<input type= 'text' id='%s' name='%s' value='%s'/>",'aqrc_width','aqrc_width',$width);
-}
+add_shortcode('button', 'button_functions');
 
-add_action("admin_init",'aqrc_settings_init');
+
+
+
+
+
+
+
+
+
+
+
+
+// function aqrc_function($content){
+//     $aqrc_post_link = get_permalink();
+//     $aqrc_title = 'out put of the fuction';
+//     $aqrc_src = sprintf('https://api.qrserver.com/v1/create-qr-code/?size=185x185&ecc=L&qzone=1&data=%s',$aqrc_post_link);
+//     $content .= sprintf("<img src='%s'>",$aqrc_src);
+//     return $content;
+// }
+// add_filter( 'the_content', 'aqrc_function');
+
+// function aqrc_settings_init(){
+
+//     add_settings_field( 'aqrc_height', __('QR_code_Height','aqrc'),'aqrc_display_height','general');
+//     add_settings_field( 'aqrc_width', __('QR_code_Width','aqrc'),'aqrc_display_width','general');
+    
+//     register_setting('general','pqrc_display_height',array('sanaitze_callback'=>'esc_attr'));
+//     register_setting('general','pqrc_display_width',array('sanaitze_callback'=>'esc_attr'));
+
+// }
+// function aqrc_display_height(){
+//     $height = get_option( 'aqrc_height');
+//     printf("<input type= 'text' id='%s' name='%s' value='%s'/>",'aqrc_height','aqrc_heith',$height);
+// };
+// function aqrc_display_width(){
+//     $width = get_option( 'aqrc_width');
+//     printf("<input type= 'text' id='%s' name='%s' value='%s'/>",'aqrc_width','aqrc_width',$width);
+// }
+
+// add_action("admin_init",'aqrc_settings_init');
 
 
 
